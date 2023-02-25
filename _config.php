e36aa431a1ae6d7d8b0bebbca983ce1b87e98720
Psycho Botz <124176508+PSYCHO-BOTZ@users.noreply.github.com>
2023-02-25 18:08:00 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/PSYCHO-BOTZ/AnimeWorldZ-api
+$api = "https://github.com/PSYCHO-BOTZ/AnimeWorldZ-api"; // https://github.com/PSYCHO-BOTZ/AnimeWorldZ-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
